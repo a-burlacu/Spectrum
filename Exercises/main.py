d11781076f4ea225a7b84ee99f2b54334232dad2
Alina <Alina.Burlacu@charter.com>
2022-07-12 17:40:02 -0600

    updated Exercise files

--- Exercises/main.py
+++ Exercises/main.py
@@ -10,6 +10,7 @@ Feed_ID = ''
 Feed_Name = ''
 Provider_ID = ''
 DAI = ''
+AltCon = ''
 AltCon_Version = ''
 _224_Feed = ''
 Notification_Buffer = ''
@@ -17,17 +18,26 @@ message = ''
 
 @app.route("/main", methods=["GET","POST"])
 def main():
-    global Feed_ID, Feed_Name, Provider_ID, DAI, AltCon_Version,_224_Feed, Notification_Buffer
+    global Feed_ID, Feed_Name, Provider_ID, DAI, AltCon, AltCon_Version,_224_Feed, Notification_Buffer
     global message
     if request.args.get('action') == "savefeed":
         Feed_ID = request.form.get("Feed_ID")
-        if add_FeedID(Feed_ID):
+        Feed_Name = request.form.get("Feed_Name")
+        Provider_ID = request.form.get("Provider_ID")
+        DAI = request.form.get("DAI")
+        AltCon = request.form.get("AltCon")
+
+        if add_feedID(Feed_ID, Feed_Name, Provider_ID, DAI, AltCon):
             message = "Successfully Added"
         else:
             message = "Failed to Add"
 
 
-    return render_template('input_form.html', Feed_ID=Feed_ID,message=message)
+    return render_template('input_form.html',
+                           Feed_ID=Feed_ID,
+                           Feed_Name=Feed_Name,
+
+                           message=message)
 
 @app.route("/view", methods=["GET"])
 def view():
@@ -38,7 +48,7 @@ def view():
 
 if __name__ == '__main__':
         print('running app...')
-        app.run(host='0.0.0.0', port='7002')
+        app.run(host='127.0.0.1', port='7002')
         print('----app is running-----')
 
 
